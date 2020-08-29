3ce56742e3c2763a102c0982eee0a4c483a5d4c3
thutgtz <35361103+thutgtz@users.noreply.github.com>
2020-08-29 22:52:54 +0700

    playercontroller with accelerometer & following camera

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 Direction = Vector3.zero;
+    public float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        //Input.gyro.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+     //   float posX = Input.gyro.attitude.x;
+     //   float posY = Input.gyro.attitude.y;
+
+        float posX = Input.acceleration.x;
+        float posY = Input.acceleration.y;
+        Direction = new Vector3(posX*5, posY*5, speed);
+        controller.Move(Direction*Time.deltaTime);
+    }
+}
