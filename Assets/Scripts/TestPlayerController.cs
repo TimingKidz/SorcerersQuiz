06f7a5010b322545126d2c5be45b713116d328e9
TimingKidz <35361317+TimingKidz@users.noreply.github.com>
2020-08-31 20:57:23 +0700

    Add Endless scene generator

---
+++ Assets/Scripts/TestPlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayerController : MonoBehaviour
+{
+    public float speed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float speedpersec = speed * Time.deltaTime;
+        transform.Translate(0.0f, 0.0f, speedpersec);
+    }
+}
