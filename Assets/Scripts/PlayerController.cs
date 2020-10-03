08128c5d94bba4b6bebdc56f7d9760c310b1ca98
TimingKidz <35361317+TimingKidz@users.noreply.github.com>
2020-10-03 16:51:43 +0700

    level stunt and question

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.0f;
     public float accelerationSpeed = 100.0f;
     private Vector3 Init = Vector3.zero;
+    public bool isStunt = false;
+    float currentTime;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +23,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isStunt)
+        {
+            speed = 0.0f;
+            if (Time.time >= currentTime + 5)
+            {
+                speed = 5.0f;
+                isStunt = false;
+            }
+        }
 
         // float posX = Input.gyro.attitude.x;
         // float posY = Input.gyro.attitude.y;
@@ -31,9 +42,17 @@ public class PlayerController : MonoBehaviour
 /*        float posX = Input.acceleration.x * accelerationSpeed;
         float posY = Input.acceleration.y * accelerationSpeed;*/
 
-        Direction = new Vector3(0,0, speed);
+        Direction = new Vector3(0, posY, speed);
         //controller.Move(Direction * Time.deltaTime);
-        transform.Translate(0, posY*Time.deltaTime, speed * Time.deltaTime);
+        Direction = transform.TransformDirection(Direction);
         transform.Rotate(0, posX*Time.deltaTime, 0);
+
+        CollisionFlags flag = controller.Move(Direction * Time.deltaTime);
+    }
+
+    public void SpeedReducer(float s)
+    {
+        currentTime = Time.time;
+        isStunt = true;
     }
 }
